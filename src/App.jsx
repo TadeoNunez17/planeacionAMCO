4bd07887f345c55a23ee2220877b405fc669f5a8
JTadeoNunezC17 <jotanunezca@ittepic.edu.mx>
2026-04-24 23:53:38 -0700

    1

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,7 @@ export default function App() {
     <div className="min-h-screen bg-slate-50 pb-20 font-sans">
       <Header />
 
-      <div className="max-w-4xl mx-auto p-4">
+      <div className="container mx-auto p-4">
         {step === 1 && (
           <StepLogistica 
             plan={plan}
